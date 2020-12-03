0bf128b506ec32b973f6fef9067d461ec60c8b73
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-03 16:04:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+// Scooter with year, color, and model properties
+function Scooter(year, color, model){
+  this.year = year 
+  this.color = color
+  this.model = model
+}
+// Driver with name, age, and experience properties
+// PickupLocation with address and city properties
